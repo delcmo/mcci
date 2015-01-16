6f15db2ab10387f87558507b9a6a6b8d05f03713
Marco Delchini <marco.delchini@gmail.com>
2015-01-16 15:05:23 -0600

    Beginning my new application

---
+++ include/base/McciApp.h
@@ -0,0 +1,22 @@
+#ifndef MCCIAPP_H
+#define MCCIAPP_H
+
+#include "MooseApp.h"
+
+class McciApp;
+
+template<>
+InputParameters validParams<McciApp>();
+
+class McciApp : public MooseApp
+{
+public:
+  McciApp(const std::string & name, InputParameters parameters);
+  virtual ~McciApp();
+
+  static void registerApps();
+  static void registerObjects(Factory & factory);
+  static void associateSyntax(Syntax & syntax, ActionFactory & action_factory);
+};
+
+#endif /* MCCIAPP_H */
